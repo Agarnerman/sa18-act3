37f9948d24af5b3a2a07acf9f46a4833cdc3800d
Antonio Garner <agarner.man@gmail.com>
2024-04-28 23:15:04 -0500

    fixed the products not showing issue

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+import decimal
 
 class Product(models.Model):
   name = models.CharField(max_length=255)
-  price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+  price = models.DecimalField(max_digits=10, decimal_places=2)
   description = models.TextField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Product
-
-def product_list(request):
-  products = Product.objects.all()
-  return render(request, 'myapp/index.html', {'products': products})
-
-def product_detail(request, product_id):
-  product = get_object_or_404(Product, pk=product_id)
-  return render(request, 'myapp/show.html', {'product': product})
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    context = {'products': products}
+    return render(request, 'myapp/index.html', context)
 
 def show(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    return render(request, 'show.html', {'product': product})
+    context = {'product': product}
+    return render(request, 'myapp/show.html', context)
+
+def home(request):
+    products = Product.objects.all() 
+    context = {'products': products}
+    return render(request, 'myapp/home.html', context)
+
+def show_product(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return render(request, 'myapp/product_detail.html', {'product': product})
